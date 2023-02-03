bb9404bd639561398c5623d4afd88bc55f448f7f
Subham Roy <subhamroy102.hitece2020@gmail.com>
2023-02-03 12:43:31 +0530

    IndexScreen Updates

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,20 @@ const Index = () => {
     <>
        <Hero/>
        <Heading/>
+       <div className='aboutusbg'>
+        <div className='pt-40 pl-24'>
+          <p className='font-[600] text-[30px]'>About Us</p>
+          <p className='text-dark font-[400] text-[14px] my-4 '>Lorem ipsum dolor sit amet consectetur. Ultrices malesuada pharetra amet egestas<br/>
+purus praesent quis. Consectetur nisi suspendisse lectus cum lorem. Netus massa <br/>
+lectus commodo bibendum feugiat. Accumsan ut id elementum diam sit nunc.<br/>
+Ullamcorper nisi nunc ultrices cras orci facilisi aenean. Sed ultrices quis leo<br/> 
+a velit fringilla sed cursus. Vestibulum duis lacus nunc in tempus et amet.<br/> 
+Feugiat elementum tincidunt arcu sed pellentesque adipiscing. Fermentum<br/> 
+diam scelerisque vulputate bibendum scelerisque.</p>
+<p className='text-white bg-main py-2 px-5 rounded-md cursor-pointer max-w-max'>Read More</p>
+        </div>
+
+       </div>
     </>
   )
 }
