4a2043713847ad64a527200b3557ed28ce8176db
Paras <paras@scus.tech>
2023-02-15 01:24:19 +0530

    language dropdown created

--- components/portfoliodetails/portfoliodetails.js
+++ components/portfoliodetails/portfoliodetails.js
@@ -47,10 +47,6 @@ const Portfoliodetails = ({ id }) => {
                           query: { id: itms.id },
                         });
 
-
-
-
-
                       }}
                       className="relative"
                     >
