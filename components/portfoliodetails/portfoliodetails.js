090a3601a96c51bcc7af61868b296f1305e27b29
Paras <paras@scus.tech>
2023-03-19 12:02:57 +0530

    Changes Commit

--- components/portfoliodetails/portfoliodetails.js
+++ components/portfoliodetails/portfoliodetails.js
@@ -134,7 +134,6 @@ const Portfoliodetails = ({ allprofiles, total }) => {
     console.log("res ", res);
   };
   // like profile code end
-
   useEffect(() => {
     issetList(false);
     issetGrid(true);
