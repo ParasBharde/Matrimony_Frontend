57c8deefc9ec8272907b98252583e3c33efd6d31
Paras <paras@scus.tech>
2023-03-19 13:34:19 +0530

    small fixes

--- components/IndexScreen/hero.js
+++ components/IndexScreen/hero.js
@@ -139,7 +139,7 @@ const Hero = (props) => {
                   className="relative max-md:right-10 right-[2rem]"
                 >
                   <Image
-                    className="rounded-full max-w-[50px]"
+                    className="rounded-full max-w-[45px]"
                     loader={imgLoader}
                     src={
                       userProfile != null
