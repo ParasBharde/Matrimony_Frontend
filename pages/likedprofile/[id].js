b691af5f4fc5d14d18b5bca6b7e5e2d2d8cf18dc
Lalit Narware <lalitnarware2001@gmail.com>
2023-04-04 17:08:19 +0530

    changes on likedprofile/[id] and header.js file

--- pages/likedprofile/[id].js
+++ pages/likedprofile/[id].js
@@ -143,7 +143,7 @@ const Likedprofile = () => {
               </div>
               <div>
                 <nav
-                  className="isolate inline-flex -space-x-px max-md:relative max-md:top-[-2rem] left-[18rem] max-md:gap-2  "
+                  className="isolate inline-flex -space-x-px max-md:relative max-md:top-[-2rem] left-[12rem] max-md:gap-2  "
                   aria-label="Pagination"
                 >
                   <span className="px-2 py-1 max-md:hidden ">View by :</span>
