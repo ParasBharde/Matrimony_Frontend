2e654155b633238d75ab48e7a70f9dd92b736f8b
Paras <paras@scus.tech>
2023-07-19 20:21:41 +0530

    Config File Added

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'Matrimony_Frontend',
+        script: '_app.js', // Entry point of your application
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+      // Add more apps here if needed
+    ],
+  };
